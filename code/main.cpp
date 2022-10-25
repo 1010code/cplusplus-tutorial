bdd1bfc2e2ada5e87a2b3bb502814e56c116d9a7
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2022-10-25 19:57:48 +0800

    add ch11

--- code/main.cpp
+++ code/main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
 using namespace std;
-namespace first
-{
-  int x = 5;
-  int y = 10; 
-}
-namespace second
-{
-  double x = 3.1416;
-  double y = 2.7183;
-}
 int main () {
-  using namespace first;
-  cout << x << '\n';
-  cout << y << '\n';
-  cout << second::x << '\n';
-  cout << second::y << '\n';
+  char myword[] = "Hello";
+  myword[6]= 'v';
   return 0; 
 }
